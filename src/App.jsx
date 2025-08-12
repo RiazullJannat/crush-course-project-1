2ac72c1a869c2a44e0684cb3e2c3f73390514524
RJannat <ramimk097@gmail.com>
2025-08-12 07:08:10 +0600

    construct toggle system

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react';
 import './App.css'
 import Nav from './components/Nav'
 import Banner from './components/Banner';
+import Developers from './components/Developers';
 
 function App() {
   let [coins, setCoins] = useState(0);
+  let [toggle, setToggle] = useState(true);
   const handleGetCoins = () => {
     setCoins(coins + 10000);
+  }
+  const handleToggle = (bool) => {
+    setToggle(bool);
   }
   return (
     <>
       <Nav coins={coins} />
-      <Banner handleGetCoins={handleGetCoins}/>
+      <Banner handleGetCoins={handleGetCoins} />
+      <Developers handleToggle={handleToggle}  toggle={toggle}/>
     </>
   )
 }
--- src/components/Developers.jsx
+++ src/components/Developers.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const Developers = ({selected}) => {
+const Developers = ({handleToggle, toggle}) => {
     return (
         <div>
+            <div className='flex justify-between'>
+                <div>
+                    <h3 className={`${toggle?'block':'hidden'}`}>Available</h3>
+                    <h3 className={`${toggle?'hidden':'false'}`}>Selected: 0/6</h3>
+                </div>
+                <div>
+                    <button onClick={()=>handleToggle(true)} className={`${toggle?"btn-success":""} btn btn-outline mr-2`}>Available</button>
+                    <button onClick={()=>handleToggle(false)} className={`${toggle?"":"btn-success"} btn btn-outline`}>Selected</button>
+                </div>
+            </div>
             <div>
-                <div>
-                    <h3>Available</h3>
-                    <h3>Selected: {selected}/6</h3>
-                </div>
-                <div>
-                    <button>Available</button>
-                    <button>Selected</button>
-                </div>
+                
             </div>
         </div>
     );
